db68eade5ea55e8640d6a5dfc640d6283de9596a
James Green <jrgreen@gmail.com>
2016-10-18 21:11:13 +0100

    Add local storage to store places entered

--- scripts/rentController.js
+++ scripts/rentController.js
@@ -1,9 +1,21 @@
 app.controller('rentController', ['$scope', '$filter', function ($scope, $filter) {
-    $scope.places = [];
     $scope.loisLatitude = 37.7716948;
     $scope.loisLongitude = -122.4405735;
     $scope.myLatitude = 51.52152280000001;
     $scope.myLongitude = -0.1420892;
+    
+    $scope.init = function () {
+        var tmpPlaces = localStorage.getItem("places");
+        if (tmpPlaces === '' || tmpPlaces === undefined || tmpPlaces === null) {
+            $scope.places = [];
+        }
+        else {
+            $scope.places = JSON.parse(tmpPlaces);
+        }
+    };
+    var updateStorage = function () {
+        localStorage.setItem("places", JSON.stringify($scope.places));
+    };
     $scope.addPlace = function (link, location, pricePerWeek, pricePerMonth, notes) {
         var newPlace = {
             "link": link
@@ -50,13 +62,13 @@ app.controller('rentController', ['$scope', '$filter', function ($scope, $filter
                 var destination = $('#destination').val();
                 var distance_text = calculateDistance(origin, destination);
             });
-
         });
-
         $scope.places = $scope.places.concat(newPlace);
+        updateStorage();
     };
     $scope.remove = function ($index) {
         $scope.places.splice($index, 1);
+        updateStorage();
     };
     $scope.change = function (whichField) {
         if (whichField === 'perweek') {
